a4706f79e84165bcb42f3a628f13cb69d49a4fe2
38061027 <joseaugustogomessantos12@gmail.com>
2024-06-28 14:47:36 -0300

    testando a aplicação

--- src/app/components/favorites/favorites.component.spec.ts
+++ src/app/components/favorites/favorites.component.spec.ts
@@ -1,12 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { FavoritesComponent } from './favorites.component';
-import {HttpClientTestingModule} from '@angular/common/http/testing'
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
 import { FooterComponent } from '../footer/footer.component';
+import { SharedService } from 'src/app/services/shared.service';
+import { of } from 'rxjs';
 
 describe('FavoritesComponent', () => {
   let component: FavoritesComponent;
   let fixture: ComponentFixture<FavoritesComponent>;
+  let httpMock: HttpTestingController
+  let service: SharedService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,7 +18,8 @@ describe('FavoritesComponent', () => {
       declarations: [ FavoritesComponent, FooterComponent ]
     })
     .compileComponents();
-
+    httpMock = TestBed.inject(HttpTestingController);
+    service = TestBed.inject(SharedService);
     fixture = TestBed.createComponent(FavoritesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -23,4 +28,15 @@ describe('FavoritesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Deve excluir favoritos e retornar favoritos', () =>{
+    const deleteSpy = spyOn(service, 'deleteFavorite').and.returnValue(of({}));
+    const getFavoritesSpy = spyOn(component, 'getFavorites').and.callThrough();
+
+    const id = '1';
+    component.deleteFavorite(id);
+
+    expect(deleteSpy).toHaveBeenCalledWith(id);
+    expect(getFavoritesSpy).toHaveBeenCalled();
+  })
 });
--- src/app/services/shared.service.spec.ts
+++ src/app/services/shared.service.spec.ts
@@ -1,18 +1,33 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SharedService } from './shared.service';
-import { HttpClientTestingModule} from '@angular/common/http/testing'
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { environment } from 'src/environments/environment';
 describe('SharedService', () => {
   let service: SharedService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[HttpClientTestingModule],
+      imports: [HttpClientTestingModule],
     });
     service = TestBed.inject(SharedService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Deve excluir filme', () => {
+    const id = '2'
+    service.removeMovie(id).subscribe(res =>{
+      expect(res).toBe({})
+    })
+
+    const request = httpMock.expectOne(`${environment.API}filmes/${id}`)
+    expect(request.request.method).toBe('DELETE')
+    expect(request.request.url).toBe(`${environment.API}filmes/${id}`)
+    
+  })
 });
